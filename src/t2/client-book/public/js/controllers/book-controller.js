36764659815c3226aec5ca3ed9ea96a25ff11266
ruanHerculano <ruannawe95@gmail.com>
2017-10-18 00:46:27 -0200

    removendo logs obsoletos

--- src/t2/client-book/public/js/controllers/book-controller.js
+++ src/t2/client-book/public/js/controllers/book-controller.js
@@ -3,8 +3,6 @@ angular.module('client-book').controller('BookController', function($scope, $htt
 
 	var promise = $http.get('http://localhost:3000/books.json');
 	promise.then(function(response) {
-		console.log('chega aqui????');
-		console.log(response.data);
 		$scope.books = response.data;
 	}).catch(function(error) {
 		console.log(error);
