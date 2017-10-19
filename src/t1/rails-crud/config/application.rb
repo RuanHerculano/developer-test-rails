7ecf0debe85b428d1e2aa20adffd76324b5bdaed
ruanHerculano <ruannawe95@gmail.com>
2017-10-19 00:08:46 -0200

    salvando configuração do rake cors

--- src/t1/rails-crud/config/application.rb
+++ src/t1/rails-crud/config/application.rb
@@ -11,16 +11,16 @@ module RailsCrud
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-
     config.middleware.use Rack::Cors do
       allow do
         origins '*'
         resource '*',
           :headers => :any,
-          :methods => [:get, :put, :patch, :options],
+          :methods => [:post, :get, :put, :patch, :options],
           :max_age => 15
       end
     end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
